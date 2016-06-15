de7ac0438b9b6067e1e79dd192e301f059d1e514
Dan Richelson <dan@launchdarkly.com>
2016-06-15 12:01:47 -0700

    [wip] change number parsing

---
+++ src/LaunchDarkly.Client/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LaunchDarkly.Client
+{
+    public static class Util
+    {
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long GetUnixTimestampMillis(DateTime dateTime)
+        {
+            return (long) (dateTime - UnixEpoch).TotalMilliseconds;
+        }
+    }
+}
