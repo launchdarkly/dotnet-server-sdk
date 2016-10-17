316a67defe6038c2bd3b2cb9b55842ffed5132ba
Dan Richelson <dan@launchdarkly.com>
2016-10-17 10:44:31 -0700

    Clean up some unused files

--- src/LaunchDarkly.Client/Properties/AssemblyInfo.cs
+++ src/LaunchDarkly.Client/Properties/AssemblyInfo.cs
@@ -25,16 +25,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("186f235c-0e7c-419a-b279-5e851e18a41a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyInformationalVersion("3.0.0-beta4")]
-//[assembly: AssemblyVersion("3.0.0")]
-//[assembly: AssemblyFileVersion("3.0.0")]
