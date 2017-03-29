06c426e3234206e165b5838c8f7812a8b51ee5ca
Igor Skomorokh <igor.skomorokh@d2l.com>
2017-03-29 15:01:47 +0300

    Added unit tests around Operator class

--- test/LaunchDarkly.Tests/OperatorTest.cs
+++ test/LaunchDarkly.Tests/OperatorTest.cs
@@ -5,8 +5,10 @@ using Xunit;
 
 namespace LaunchDarkly.Tests
 {
+
     public class OperatorTest
     {
+
         [Fact]
         public void CanParseUtcTimestamp()
         {
@@ -65,5 +67,202 @@ namespace LaunchDarkly.Tests
             Assert.True(Operator.Apply("before", before, after));
             Assert.False(Operator.Apply("before", after, before));
         }
+
+        [Fact]
+        public void Apply_UnknownOperation_ReturnsFalse()
+        {
+            Assert.False(Operator.Apply("unknown", new JValue(10), new JValue(10)));
+        }
+
+        [Fact]
+        public void Apply_UserValueIsNull_ReturnsFalse()
+        {
+            Assert.False(Operator.Apply("in", null, new JValue(10)));
+        }
+
+        [Fact]
+        public void Apply_ClauseValueIsNull_ReturnsFalse()
+        {
+            Assert.False(Operator.Apply("in", new JValue(10), null));
+        }
+
+        [Theory]
+        [InlineData(10, 10)]
+        [InlineData("hello", "hello")]
+        [InlineData(12.5d, 12.5d)]
+        [InlineData(11, 11d)]
+        [InlineData(11d, 11)]
+        public void Apply_In_SupportedTypes_ValuesAreEqual_ReturnsTrue(object userValue, object clauseValue)
+        {
+            Assert.True(Operator.Apply("in", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(11, 60)]
+        [InlineData("first", "second")]
+        [InlineData(745.4d, 92.5d)]
+        [InlineData(67, 92.5d)]
+        [InlineData(11.4d, 11)]
+        public void Apply_In_SupportedTypes_ValuesAreNotEqual_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("in", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData("userValue", "Value")]
+        [InlineData("userValue", "userValue")]
+        public void Apply_EndsWith_SupportedTypes_ReturnsTrue(string userValue, string clauseValue)
+        {
+            Assert.True(Operator.Apply("endsWith", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData("userValue", "blah")]
+        [InlineData("userValue", 77)]
+        [InlineData(78, "userValue")]
+        public void Apply_EndsWith_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("endsWith", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData("userValue", "userV")]
+        [InlineData("userValue", "user")]
+        public void Apply_StartsWith_SupportedTypes_ReturnsTrue(string userValue, string clauseValue)
+        {
+            Assert.True(Operator.Apply("startsWith", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData("userValue", "blah")]
+        [InlineData("userValue", 77)]
+        [InlineData(78, "userValue")]
+        public void Apply_StartsWith_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("startsWith", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Fact]
+        public void Apply_Matches_SupportedTypes_ReturnsTrue()
+        {
+            Assert.True(Operator.Apply("matches", new JValue("22"), new JValue(@"\d")));
+        }
+
+        [Theory]
+        [InlineData("Some text", @"\d")]
+        [InlineData(@"\d", "22")]
+        [InlineData("userValue", 77)]
+        [InlineData(77, "userValue")]
+        public void Apply_Matches_SupportedTypes_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("matches", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Fact]
+        public void Apply_Contains_SupportedTypes_ReturnsTrue()
+        {
+            Assert.True(Operator.Apply("contains", new JValue("userValue"), new JValue("serValu")));
+        }
+
+        [Theory]
+        [InlineData("userValue", "blah")]
+        [InlineData("userValue", 77)]
+        [InlineData(78, "userValue")]
+        public void Apply_Contains_SupportedTypes_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("contains", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(10, 11)]
+        [InlineData(55d, 66)]
+        public void Apply_LessThan_SupportedTypes_ReturnsTrue(object userValue, object clauseValue)
+        {
+            Assert.True(Operator.Apply("lessThan", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(10, 10)]
+        [InlineData(10, 9)]
+        [InlineData(55d, 55)]
+        [InlineData(55d, 54)]
+        [InlineData(55, 55d)]
+        [InlineData(55, 54d)]
+        [InlineData(55d, 55d)]
+        [InlineData(55d, 54d)]
+        public void Apply_LessThan_SupportedTypes_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("lessThan", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(99, 99)]
+        [InlineData(77, 78)]
+        [InlineData(46d, 46)]
+        [InlineData(33d, 34d)]
+        [InlineData(33d, 33d)]
+        [InlineData(32, 34d)]
+        public void Apply_LessThanOrEqual_SupportedTypes_ReturnsTrue(object userValue, object clauseValue)
+        {
+            Assert.True(Operator.Apply("lessThanOrEqual", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(999, 99)]
+        [InlineData(79, 78)]
+        [InlineData(46d, 45)]
+        [InlineData(33d, 32d)]
+        [InlineData(23, 22d)]
+        public void Apply_LessThanOrEqual_SupportedTypes_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("lessThanOrEqual", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(11, 10)]
+        [InlineData(66, 55d)]
+        public void Apply_GreaterThan_SupportedTypes_ReturnsTrue(object userValue, object clauseValue)
+        {
+            Assert.True(Operator.Apply("greaterThan", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(10, 10)]
+        [InlineData(9, 10)]
+        [InlineData(55, 55d)]
+        [InlineData(54, 55d)]
+        [InlineData(55d, 55)]
+        [InlineData(54d, 55)]
+        [InlineData(55d, 55d)]
+        [InlineData(54d, 55d)]
+        public void Apply_GreaterThan_SupportedTypes_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("greaterThan", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(99, 99)]
+        [InlineData(78, 77)]
+        [InlineData(46, 46d)]
+        [InlineData(34d, 33d)]
+        [InlineData(33d, 33d)]
+        [InlineData(34d, 32)]
+        public void Apply_GreaterThanOrEqual_SupportedTypes_ReturnsTrue(object userValue, object clauseValue)
+        {
+            Assert.True(Operator.Apply("greaterThanOrEqual", new JValue(userValue), new JValue(clauseValue)));
+        }
+
+        [Theory]
+        [InlineData(99, 999)]
+        [InlineData(78, 79)]
+        [InlineData(45, 46d)]
+        [InlineData(32d, 33d)]
+        [InlineData(22d, 23)]
+        public void Apply_GreaterThanOrEqual_SupportedTypes_ReturnsFalse(object userValue, object clauseValue)
+        {
+            Assert.False(Operator.Apply("greaterThanOrEqual", new JValue(userValue), new JValue(clauseValue)));
+        }
+
     }
+
 }
