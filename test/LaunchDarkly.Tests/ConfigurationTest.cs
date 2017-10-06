8822742d5eb2e3bb89194366158737da8bf79d7e
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2017-10-06 15:33:14 -0700

    add stream configuration unit tests

--- test/LaunchDarkly.Tests/ConfigurationTest.cs
+++ test/LaunchDarkly.Tests/ConfigurationTest.cs
@@ -22,6 +22,22 @@ namespace LaunchDarkly.Tests
         }
 
         [Fact]
+        public void CanOverrideStreamConfiguration()
+        {
+            var config = Configuration.Default("AnyOtherSdkKey")
+                .WithStreamUri("https://stream.AnyOtherEndpoint.com")
+                .WithIsStreamingEnabled(false)
+                .WithIsRelayEnabled(true)
+                .WithReadTimeout(TimeSpan.FromDays(1))
+                .WithReconnectTime(TimeSpan.FromDays(1));
+
+            Assert.Equal(new Uri("https://stream.AnyOtherEndpoint.com"), config.StreamUri);
+            Assert.Equal(false, config.IsStreamingEnabled);
+            Assert.Equal(true, config.IsRelayEnabled);
+            Assert.Equal(TimeSpan.FromDays(1), config.ReadTimeout);
+            Assert.Equal(TimeSpan.FromDays(1), config.ReconnectTime);
+        }
+        [Fact]
         public void CannotOverrideTooSmallPollingInterval()
         {
             var config = Configuration.Default("AnyOtherSdkKey")
