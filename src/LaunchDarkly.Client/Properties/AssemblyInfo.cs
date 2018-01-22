8c61aa4c0022059d56defc4a714cfc9ec487739f
Eli Bishop <eli@launchdarkly.com>
2018-01-22 14:02:32 -0800

    internals visible to tests

--- src/LaunchDarkly.Client/Properties/AssemblyInfo.cs
+++ src/LaunchDarkly.Client/Properties/AssemblyInfo.cs
@@ -23,4 +23,9 @@ using System.Runtime.InteropServices;
 [assembly: Guid("186f235c-0e7c-419a-b279-5e851e18a41a")]
 
 // Allow unit tests to see internal classes
-[assembly: InternalsVisibleTo("LaunchDarkly.Tests")]
+[assembly: InternalsVisibleTo("LaunchDarkly.Tests,PublicKey=" +
+"0024000004800000940000000602000000240000525341310004000001000100f121bbf427e4d7" +
+"edc64131a9efeefd20978dc58c285aa6f548a4282fc6d871fbebeacc13160e88566f427497b625" +
+"56bf7ff01017b0f7c9de36869cc681b236bc0df0c85927ac8a439ecb7a6a07ae4111034e03042c" +
+"4b1569ebc6d3ed945878cca97e1592f864ba7cc81a56b8668a6d7bbe6e44c1279db088b0fdcc35" +
+"52f746b4")]
