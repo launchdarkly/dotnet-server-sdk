22d4cdc6e40168cf56ff7b7563cd623110eae271
Eli Bishop <eli@launchdarkly.com>
2018-02-22 16:57:55 -0800

    make event classes and IStoreEvent interface public again

--- src/LaunchDarkly.Client/IStoreEvents.cs
+++ src/LaunchDarkly.Client/IStoreEvents.cs
@@ -1,8 +1,20 @@
 ﻿namespace LaunchDarkly.Client
 {
-    internal interface IStoreEvents
+    /// <summary>
+    /// Interface for an object that sends or stores analytics events. The default implementation
+    /// sends all analytics events to LaunchDarkly.
+    /// </summary>
+    public interface IStoreEvents
     {
+        /// <summary>
+        /// Processes an event.
+        /// </summary>
+        /// <param name="eventToLog">the event</param>
         void Add(Event eventToLog);
+
+        /// <summary>
+        /// If events are being buffered, flushes the buffer.
+        /// </summary>
         void Flush();
     }
 }
