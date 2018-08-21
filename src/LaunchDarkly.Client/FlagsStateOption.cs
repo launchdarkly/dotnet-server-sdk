e9861fe9da3f20093247346e613ca225cec91a60
Eli Bishop <eli@launchdarkly.com>
2018-08-20 17:48:50 -0700

    add option to select only client-side flags in AllFlagsState()

---
+++ src/LaunchDarkly.Client/FlagsStateOption.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaunchDarkly.Client
+{
+    /// <summary>
+    /// Optional parameters that can be passed to <see cref="ILdClient.AllFlagsState(User, FlagsStateOption[])"/>.
+    /// </summary>
+    public class FlagsStateOption
+    {
+        private readonly string _description;
+
+        private FlagsStateOption(string description)
+        {
+            _description = description;
+        }
+
+        /// <summary>
+        /// Returns the string representation of this option.
+        /// </summary>
+        /// <returns>the string representation</returns>
+        public override string ToString()
+        {
+            return _description;
+        }
+
+        /// <summary>
+        /// Specifies that only flags m arked for use with the client-side SDK should be included in the
+        /// state object. By default, all flags are included.
+        /// </summary>
+        public static readonly FlagsStateOption ClientSideOnly = new FlagsStateOption("ClientSideOnly");
+
+        internal static bool HasOption(FlagsStateOption[]  options, FlagsStateOption option)
+        {
+            foreach (var o in options)
+            {
+                if (o == option)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
