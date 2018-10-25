b18d45121ea0a02fe72420fd1f25473e7011075a
Eli Bishop <eli@launchdarkly.com>
2018-10-24 21:18:12 -0700

    comments

--- src/LaunchDarkly.Client/Files/FlagFactory.cs
+++ src/LaunchDarkly.Client/Files/FlagFactory.cs
@@ -2,6 +2,10 @@
 
 namespace LaunchDarkly.Client.Files
 {
+    // Creates flag or segment objects from raw JSON.  Note that the FeatureFlag and Segment
+    // classes are internal to LaunchDarkly.Client, so we refer to those types indirectly via
+    // VersionedDataKind; and if we want to construct a flag from scratch, we can't use the
+    // constructor but instead must build some JSON and then parse that.
     internal static class FlagFactory
     {
         public static IVersionedData FlagFromJson(JToken json)
@@ -9,6 +13,8 @@ namespace LaunchDarkly.Client.Files
             return json.ToObject(VersionedDataKind.Features.GetItemType()) as IVersionedData;
         }
 
+        // Constructs a flag that always returns the same value. This is done by giving it a
+        // single variation and setting the fallthrough variation to that.
         public static IVersionedData FlagWithValue(string key, JToken value)
         {
             var o = new JObject();
