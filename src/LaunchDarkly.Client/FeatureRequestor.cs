604319a47a00d53114c9f5339a8aaee76892bc46
Eli Bishop <eli@launchdarkly.com>
2019-03-22 16:10:54 -0700

    remove retry for poll requests

--- src/LaunchDarkly.Client/FeatureRequestor.cs
+++ src/LaunchDarkly.Client/FeatureRequestor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -48,17 +47,12 @@ namespace LaunchDarkly.Client
         // was a problem getting flags.
         async Task<AllData> IFeatureRequestor.GetAllDataAsync()
         {
-            var cts = new CancellationTokenSource(_config.HttpClientTimeout);
-            string content = null;
-            content = await Get(cts, _allUri);
-            if (string.IsNullOrEmpty(content)) {
-                return null;
+            var ret = await GetAsync<AllData>(_allUri);
+            if (ret != null)
+            {
+                Log.DebugFormat("Get all returned {0} feature flags and {1} segments",
+                    ret.Flags.Keys.Count, ret.Segments.Keys.Count);
             }
-            var ret = JsonConvert.DeserializeObject<AllData>(content);
-
-            Log.DebugFormat("Get all returned {0} feature flags and {1} segments",
-                ret.Flags.Keys.Count, ret.Segments.Keys.Count);
-
             return ret;
         }
 
@@ -66,37 +60,45 @@ namespace LaunchDarkly.Client
         // was a problem getting flags.
         async Task<FeatureFlag> IFeatureRequestor.GetFlagAsync(string featureKey)
         {
-            return await GetObjectAsync<FeatureFlag>(featureKey, "feature flag", typeof(FeatureFlag), _flagsUri);
+            return await GetAsync<FeatureFlag>(new Uri(_flagsUri, featureKey));
         }
 
         // Returns the latest version of a segment, or null if it has not been modified. Throws an exception if there
         // was a problem getting segments.
         async Task<Segment> IFeatureRequestor.GetSegmentAsync(string segmentKey)
         {
-            return await GetObjectAsync<Segment>(segmentKey, "segment", typeof(Segment), _segmentsUri);
+            return await GetAsync<Segment>(new Uri(_segmentsUri, segmentKey));
         }
-
-        internal async Task<T> GetObjectAsync<T>(string key, string objectName, Type objectType, Uri uriBase) where T : class
+        
+        private async Task<T> GetAsync<T>(Uri path) where T : class
         {
-            var cts = new CancellationTokenSource(_config.HttpClientTimeout);
-            string content = null;
-            Uri apiPath = new Uri(uriBase + key);
-            try
+            Log.DebugFormat("Getting flags with uri: {0}", path.AbsoluteUri);
+            var request = new HttpRequestMessage(HttpMethod.Get, path);
+            if (_etag != null)
             {
-                content = await Get(cts, apiPath);
-                return (content == null) ? null : (T) JsonConvert.DeserializeObject(content, objectType);
+                request.Headers.IfNoneMatch.Add(_etag);
             }
-            catch (Exception e)
-            {
-                Log.DebugFormat("Error getting {0}: {1} waiting 1 second before retrying.",
-                    e, objectName, Util.ExceptionMessage(e));
 
-                System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(1)).Wait();
-                cts = new CancellationTokenSource(_config.HttpClientTimeout);
+            using (var cts = new CancellationTokenSource(_config.HttpClientTimeout))
+            {
                 try
                 {
-                    content = await Get(cts, apiPath);
-                    return (content == null) ? null : (T)JsonConvert.DeserializeObject(content, objectType);
+                    using (var response = await _httpClient.SendAsync(request, cts.Token).ConfigureAwait(false))
+                    {
+                        if (response.StatusCode == HttpStatusCode.NotModified)
+                        {
+                            Log.Debug("Get all flags returned 304: not modified");
+                            return null;
+                        }
+                        _etag = response.Headers.ETag;
+                        //We ensure the status code after checking for 304, because 304 isn't considered success
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            throw new UnsuccessfulResponseException((int)response.StatusCode);
+                        }
+                        var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        return string.IsNullOrEmpty(content) ? null : (T)JsonConvert.DeserializeObject<T>(content);
+                    }
                 }
                 catch (TaskCanceledException tce)
                 {
@@ -106,39 +108,9 @@ namespace LaunchDarkly.Client
                         throw;
                     }
                     //Otherwise this was a request timeout.
-                    throw new TimeoutException("Get item with URL: " + apiPath +
+                    throw new TimeoutException("Get item with URL: " + path.AbsoluteUri +
                                                 " timed out after : " + _config.HttpClientTimeout);
                 }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-        }
-
-        private async Task<string> Get(CancellationTokenSource cts, Uri path)
-        {
-            Log.DebugFormat("Getting flags with uri: {0}", path.AbsoluteUri);
-            var request = new HttpRequestMessage(HttpMethod.Get, path);
-            if (_etag != null)
-            {
-                request.Headers.IfNoneMatch.Add(_etag);
-            }
-
-            using (var response = await _httpClient.SendAsync(request, cts.Token).ConfigureAwait(false))
-            {
-                if (response.StatusCode == HttpStatusCode.NotModified)
-                {
-                    Log.Debug("Get all flags returned 304: not modified");
-                    return null;
-                }
-                _etag = response.Headers.ETag;
-                //We ensure the status code after checking for 304, because 304 isn't considered success
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new UnsuccessfulResponseException((int)response.StatusCode);
-                }
-                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
         }
     }
