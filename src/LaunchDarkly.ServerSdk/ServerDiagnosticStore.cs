0844cb8518b3c8d7df27a4e77e7dae6278313894
Gavin Whelan <gwhelan@launchdarkly.com>
2019-09-10 20:53:41 +0000

    Update server diagnostic store for IDiagnosticStore updates.

--- src/LaunchDarkly.ServerSdk/ServerDiagnosticStore.cs
+++ src/LaunchDarkly.ServerSdk/ServerDiagnosticStore.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using LaunchDarkly.Common;
 
 namespace LaunchDarkly.Client {
@@ -6,10 +7,12 @@ namespace LaunchDarkly.Client {
     internal class ServerDiagnosticStore : IDiagnosticStore {
         readonly DiagnosticId DiagnosticId;
         DateTime DataSince;
+        long DroppedEvents;
+        long DeduplicatedUsers;
 
         DiagnosticId IDiagnosticStore.DiagnosticId => DiagnosticId;
         bool IDiagnosticStore.SendInitEvent { get; } = true;
-        DiagnosticEvent IDiagnosticStore.LastStats { get; } = null;
+        Dictionary<string, Object> IDiagnosticStore.LastStats { get; } = null;
         DateTime IDiagnosticStore.DataSince => DataSince;
 
         internal ServerDiagnosticStore(string sdkKey) {
@@ -17,14 +20,31 @@ namespace LaunchDarkly.Client {
             DiagnosticId = new DiagnosticId(sdkKey, Guid.NewGuid());
         }
 
-        public StatisticsDiagnosticEvent CreateEventAndReset(long droppedEvents, long deduplicatedUsers, long eventsInQueue)
+        public void IncrementDeduplicatedUsers() {
+            this.DeduplicatedUsers++;
+        }
+
+        public void IncrementDroppedEvents() {
+            this.DroppedEvents++;
+        }
+
+        public Dictionary<string, Object> GetStatsAndReset(long eventsInQueue)
         {
-            DateTime currentTime = DateTime.UtcNow;
-            StatisticsDiagnosticEvent res = new StatisticsDiagnosticEvent(Util.GetUnixTimestampMillis(currentTime), DiagnosticId,
-                                                                            Util.GetUnixTimestampMillis(DataSince), droppedEvents,
-                                                                            deduplicatedUsers, eventsInQueue);
-            DataSince = currentTime;
-            return res;
+            DateTime CurrentTime = DateTime.UtcNow;
+            Dictionary<string, Object> stats = new Dictionary<string, Object>();
+            stats["droppedEvents"] = DroppedEvents;
+            stats["deduplicatedUsers"] = DeduplicatedUsers;
+            stats["eventsInQueue"] = eventsInQueue;
+            stats["dataSinceDate"] = Util.GetUnixTimestampMillis(DataSince);
+            stats["currentTime"] = Util.GetUnixTimestampMillis(CurrentTime);
+            stats["id"] = DiagnosticId;
+            stats["kind"] = "diagnostic";
+
+            DataSince = CurrentTime;
+            DroppedEvents = 0;
+            DeduplicatedUsers = 0;
+
+            return stats;
         }
     }
 }
