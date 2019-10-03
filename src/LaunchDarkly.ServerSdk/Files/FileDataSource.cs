014c0a8224c9f95db31af05c9a98d0cf59bfd5ba
Eli Bishop <eli@launchdarkly.com>
2019-10-03 16:45:12 -0700

    comments explaining atomic file update issues

--- src/LaunchDarkly.ServerSdk/Files/FileDataSource.cs
+++ src/LaunchDarkly.ServerSdk/Files/FileDataSource.cs
@@ -123,7 +123,7 @@ namespace LaunchDarkly.Client.Files
 #else
                     Thread.Sleep(delay);
 #endif
-                    // Retry immediately the first time but 200ms there after
+                    // Retry immediately the first time but 200ms thereafter
                     delay = ReadFileRetryDelay;
                 }
             }
@@ -131,6 +131,10 @@ namespace LaunchDarkly.Client.Files
 
         private static bool IsFileLocked(IOException exception)
         {
+            // We cannot guarantee that these HResult values will be present on non-Windows OSes. However, this
+            // logic is less important on other platforms, because in Unix-like OSes you can atomically replace a
+            // file's contents (by creating a temporary file and then renaming it to overwrite the original file),
+            // so FileDataSource will not try to read an incomplete update; that is not possibble in Windows.
             int errorCode = exception.HResult & 0xffff;
             switch (errorCode)
             {
