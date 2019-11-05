29662e6375be00d84847b52e21a8394d02a8033b
Gavin Whelan <gwhelan@launchdarkly.com>
2019-11-05 19:58:49 +0000

    Alphabetize methods in ConfigurationBuilder.

--- src/LaunchDarkly.ServerSdk/ConfigurationBuilder.cs
+++ src/LaunchDarkly.ServerSdk/ConfigurationBuilder.cs
@@ -51,6 +51,35 @@ namespace LaunchDarkly.Client
         IConfigurationBuilder BaseUri(Uri baseUri);
 
         /// <summary>
+        ///   Set to true to opt out of sending diagnostic events.
+        /// </summary>
+        /// <remarks>
+        ///   <para>
+        ///     Unless the diagnosticOptOut field is set to true, the client will send some
+        ///     diagnostics data to the LaunchDarkly servers in order to assist in the development
+        ///     of future SDK improvements. These diagnostics consist of an initial payload
+        ///     containing some details of SDK in use, the SDK's configuration, and the platform the
+        ///     SDK is being run on; as well as payloads sent periodically with information on
+        ///     irregular occurrences such as dropped events
+        ///   </para>
+        /// </remarks>
+        /// <param name="diagnosticOptOut">true to disable diagnostic events</param>
+        /// <returns>the same builder</returns>
+        IConfigurationBuilder DiagnosticOptOut(bool diagnosticOptOut);
+
+        /// <summary>
+        ///   Sets the interval at which periodic diagnostic events will be sent.
+        /// </summary>
+        /// <remarks>
+        ///   <para>
+        ///     The default is every 15 minutes and the minimum is every minute.
+        ///   </para>
+        /// </remarks>
+        /// <param name="diagnosticRecordingInterval">the diagnostic recording interval</param>
+        /// <returns>the same builder</returns>
+        IConfigurationBuilder DiagnosticRecordingInterval(TimeSpan diagnosticRecordingInterval);
+
+        /// <summary>
         /// Sets the capacity of the events buffer.
         /// </summary>
         /// <remarks>
@@ -258,35 +287,6 @@ namespace LaunchDarkly.Client
         IConfigurationBuilder UserKeysFlushInterval(TimeSpan userKeysFlushInterval);
 
         /// <summary>
-        ///   Sets the interval at which periodic diagnostic events will be sent.
-        /// </summary>
-        /// <remarks>
-        ///   <para>
-        ///     The default is every 15 minutes and the minimum is every minute.
-        ///   </para>
-        /// </remarks>
-        /// <param name="diagnosticRecordingInterval">the diagnostic recording interval</param>
-        /// <returns>the same builder</returns>
-        IConfigurationBuilder DiagnosticRecordingInterval(TimeSpan diagnosticRecordingInterval);
-
-        /// <summary>
-        ///   Set to true to opt out of sending diagnostic events.
-        /// </summary>
-        /// <remarks>
-        ///   <para>
-        ///     Unless the diagnosticOptOut field is set to true, the client will send some
-        ///     diagnostics data to the LaunchDarkly servers in order to assist in the development
-        ///     of future SDK improvements. These diagnostics consist of an initial payload
-        ///     containing some details of SDK in use, the SDK's configuration, and the platform the
-        ///     SDK is being run on; as well as payloads sent periodically with information on
-        ///     irregular occurrences such as dropped events
-        ///   </para>
-        /// </remarks>
-        /// <param name="diagnosticOptOut">true to disable diagnostic events</param>
-        /// <returns>the same builder</returns>
-        IConfigurationBuilder DiagnosticOptOut(bool diagnosticOptOut);
-
-        /// <summary>
         /// For use by wrapper libraries to set an identifying name for the wrapper being used. This
         /// will be sent in request headers during requests to the LaunchDarkly servers to allow
         /// recording metrics on the usage of these wrapper libraries.
@@ -311,8 +311,8 @@ namespace LaunchDarkly.Client
         // Let's try to keep these properties and methods alphabetical so they're easy to find
         internal bool _allAttributesPrivate = false;
         internal Uri _baseUri = Configuration.DefaultUri;
-        internal TimeSpan _diagnosticRecordingInterval = Configuration.DefaultDiagnosticRecordingInterval;
         internal bool _diagnosticOptOut = false;
+        internal TimeSpan _diagnosticRecordingInterval = Configuration.DefaultDiagnosticRecordingInterval;
         internal int _eventCapacity = Configuration.DefaultEventQueueCapacity;
         internal TimeSpan _eventFlushInterval = Configuration.DefaultEventQueueFrequency;
         internal IEventProcessorFactory _eventProcessorFactory = null;
@@ -348,8 +348,8 @@ namespace LaunchDarkly.Client
         {
             _allAttributesPrivate = copyFrom.AllAttributesPrivate;
             _baseUri = copyFrom.BaseUri;
-            _diagnosticRecordingInterval = copyFrom.DiagnosticRecordingInterval;
             _diagnosticOptOut = copyFrom.DiagnosticOptOut;
+            _diagnosticRecordingInterval = copyFrom.DiagnosticRecordingInterval;
             _eventCapacity = copyFrom.EventCapacity;
             _eventFlushInterval = copyFrom.EventFlushInterval;
             _eventProcessorFactory = copyFrom.EventProcessorFactory;
@@ -394,6 +394,26 @@ namespace LaunchDarkly.Client
         public IConfigurationBuilder BaseUri(Uri baseUri)
         {
             _baseUri = baseUri;
+            return this;
+        }
+
+        public IConfigurationBuilder DiagnosticOptOut(bool diagnosticOptOut)
+        {
+            _diagnosticOptOut = diagnosticOptOut;
+            return this;
+        }
+
+        public IConfigurationBuilder DiagnosticRecordingInterval(TimeSpan diagnosticRecordingInterval)
+        {
+            if (diagnosticRecordingInterval.CompareTo(Configuration.MinimumDiagnosticRecordingInterval) < 0)
+            {
+                Log.Warn("DiagnosticRecordingInterval cannot be less than the minimum of 1 minute.");
+                _diagnosticRecordingInterval = Configuration.MinimumDiagnosticRecordingInterval;
+            }
+            else
+            {
+                _diagnosticRecordingInterval = diagnosticRecordingInterval;
+            }
             return this;
         }
 
@@ -532,26 +552,6 @@ namespace LaunchDarkly.Client
         public IConfigurationBuilder UserKeysFlushInterval(TimeSpan userKeysFlushInterval)
         {
             _userKeysFlushInterval = userKeysFlushInterval;
-            return this;
-        }
-
-        public IConfigurationBuilder DiagnosticRecordingInterval(TimeSpan diagnosticRecordingInterval)
-        {
-            if (diagnosticRecordingInterval.CompareTo(Configuration.MinimumDiagnosticRecordingInterval) < 0)
-            {
-                Log.Warn("DiagnosticRecordingInterval cannot be less than the minimum of 1 minute.");
-                _diagnosticRecordingInterval = Configuration.MinimumDiagnosticRecordingInterval;
-            }
-            else
-            {
-                _diagnosticRecordingInterval = diagnosticRecordingInterval;
-            }
-            return this;
-        }
-
-        public IConfigurationBuilder DiagnosticOptOut(bool diagnosticOptOut)
-        {
-            _diagnosticOptOut = diagnosticOptOut;
             return this;
         }
 
