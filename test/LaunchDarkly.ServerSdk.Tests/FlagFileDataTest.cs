ee899e57a376451d002f04f59d81341bbce2c72f
Jeff Ashton <Jeff.Ashton@D2L.com>
2019-12-04 14:23:20 -0500

    Providing option to overwrite duplicate keys in FileDataSource to allow for graceful file rolling.

---
+++ test/LaunchDarkly.ServerSdk.Tests/FlagFileDataTest.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using LaunchDarkly.Client;
+using LaunchDarkly.Client.Files;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace LaunchDarkly.Tests
+{
+    public class FlagFileDataTest
+    {
+        [Fact]
+        public void AddToData_DuplicateKeysHandling_Throw()
+        {
+            const string key = "flag1";
+
+            FeatureFlag initialFeatureFlag = new FeatureFlag(key, version: 0, deleted: false);
+
+            Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>> data =
+                new Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>>
+                {
+                    {
+                        VersionedDataKind.Features,
+                        new Dictionary<string, IVersionedData>
+                        {
+                            { key, initialFeatureFlag}
+                        }
+                    }
+                };
+
+            FlagFileData fileData = new FlagFileData
+            {
+                Flags = new Dictionary<string, JToken>
+                {
+                    {
+                        key,
+                        new JObject(
+                            new JProperty("key", key),
+                            new JProperty("version", 1)
+                        )
+                    }
+                }
+            };
+
+            Exception err = Assert.Throws<Exception>(() =>
+            {
+                fileData.AddToData(data, DuplicateKeysHandling.Throw);
+            });
+            Assert.Equal("in \"features\", key \"flag1\" was already defined", err.Message);
+
+            IVersionedData postFeatureFlag = data[VersionedDataKind.Features][key];
+            Assert.Same(initialFeatureFlag, postFeatureFlag);
+            Assert.Equal(0, postFeatureFlag.Version);
+        }
+
+        [Fact]
+        public void AddToData_DuplicateKeysHandling_OverwriteWithNewerVersion()
+        {
+            const string key = "flag1";
+
+            FeatureFlag initialFeatureFlag = new FeatureFlag(key, version: 0, deleted: false);
+
+            Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>> data =
+                new Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>>
+                {
+                    {
+                        VersionedDataKind.Features,
+                        new Dictionary<string, IVersionedData>
+                        {
+                            { key, initialFeatureFlag}
+                        }
+                    }
+                };
+
+            FlagFileData fileData = new FlagFileData
+            {
+                Flags = new Dictionary<string, JToken>
+                {
+                    {
+                        key,
+                        new JObject(
+                            new JProperty("key", key),
+                            new JProperty("version", 1)
+                        )
+                    }
+                }
+            };
+
+            fileData.AddToData(data, DuplicateKeysHandling.OverwriteIfNewerVersion);
+
+            IVersionedData postFeatureFlag = data[VersionedDataKind.Features][key];
+            Assert.NotSame(initialFeatureFlag, postFeatureFlag);
+            Assert.Equal(1, postFeatureFlag.Version);
+        }
+
+        [Fact]
+        public void AddToData_DuplicateKeysHandling_IgnoreOlderVersion()
+        {
+            const string key = "flag1";
+
+            FeatureFlag initialFeatureFlag = new FeatureFlag(key, version: 1, deleted: false);
+
+            Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>> data =
+                new Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>>
+                {
+                    {
+                        VersionedDataKind.Features,
+                        new Dictionary<string, IVersionedData>
+                        {
+                            { key, initialFeatureFlag}
+                        }
+                    }
+                };
+
+            FlagFileData fileData = new FlagFileData
+            {
+                Flags = new Dictionary<string, JToken>
+                {
+                    {
+                        key,
+                        new JObject(
+                            new JProperty("key", key),
+                            new JProperty("version", 0)
+                        )
+                    }
+                }
+            };
+
+            fileData.AddToData(data, DuplicateKeysHandling.OverwriteIfNewerVersion);
+
+            IVersionedData postFeatureFlag = data[VersionedDataKind.Features][key];
+            Assert.Same(initialFeatureFlag, postFeatureFlag);
+            Assert.Equal(1, postFeatureFlag.Version);
+        }
+
+        [Fact]
+        public void AddToData_DuplicateKeysHandling_IgnoreSameVersion()
+        {
+            const string key = "flag1";
+
+            FeatureFlag initialFeatureFlag = new FeatureFlag(key, version: 1, deleted: false);
+
+            Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>> data =
+                new Dictionary<IVersionedDataKind, IDictionary<string, IVersionedData>>
+                {
+                    {
+                        VersionedDataKind.Features,
+                        new Dictionary<string, IVersionedData>
+                        {
+                            { key, initialFeatureFlag}
+                        }
+                    }
+                };
+
+            FlagFileData fileData = new FlagFileData
+            {
+                Flags = new Dictionary<string, JToken>
+                {
+                    {
+                        key,
+                        new JObject(
+                            new JProperty("key", key),
+                            new JProperty("version", 1)
+                        )
+                    }
+                }
+            };
+
+            fileData.AddToData(data, DuplicateKeysHandling.OverwriteIfNewerVersion);
+
+            IVersionedData postFeatureFlag = data[VersionedDataKind.Features][key];
+            Assert.Same(initialFeatureFlag, postFeatureFlag);
+            Assert.Equal(1, postFeatureFlag.Version);
+        }
+    }
+}
