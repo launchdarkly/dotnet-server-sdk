5825140ec45f85b5b3f38be4e7ab795c791061c2
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-05-28 14:30:47 -0700

    More forgiving test execution.

--- pkgs/sdk/server/test/AssertHelpers.cs
+++ pkgs/sdk/server/test/AssertHelpers.cs
@@ -3,7 +3,6 @@ using LaunchDarkly.Logging;
 using LaunchDarkly.TestHelpers;
 using Xunit;
 using Xunit.Sdk;
-
 using static LaunchDarkly.Sdk.Server.Subsystems.DataStoreTypes;
 using static LaunchDarkly.TestHelpers.JsonAssertions;
 
@@ -36,7 +35,8 @@ namespace LaunchDarkly.Sdk.Server
             }
         }
 
-        private static void ThrowLogMatchException(LogCapture logCapture, bool shouldHave, LogLevel level, string text, bool isRegex) =>
+        private static void ThrowLogMatchException(LogCapture logCapture, bool shouldHave, LogLevel level, string text,
+            bool isRegex) =>
             throw new AssertActualExpectedException(shouldHave, !shouldHave,
                 string.Format("Expected log {0} the {1} \"{2}\" at level {3}\n\nActual log output follows:\n{4}",
                     shouldHave ? "to have" : "not to have",
@@ -45,22 +45,38 @@ namespace LaunchDarkly.Sdk.Server
                     level,
                     logCapture.ToString()));
 
-        public static void ExpectPredicate<T>(EventSink<T> sink, Predicate<T> predicate, string message, TimeSpan timeout)
+        /// <summary>
+        /// Expect that the given sink will receive an event that passes the provided predicate within the specified
+        /// timeout.
+        ///
+        /// The total time for the execution of this method may be greater than the timeout, because its implementation
+        /// depends on a function which itself has a timeout.
+        /// </summary>
+        /// <param name="sink">the sink to check events from</param>
+        /// <param name="predicate">the predicate to run against events</param>
+        /// <param name="message">the message to show if the test fails</param>
+        /// <param name="timeout">the overall timeout</param>
+        /// <typeparam name="T">the type of the sink and predicate</typeparam>
+        public static void ExpectPredicate<T>(EventSink<T> sink, Predicate<T> predicate, string message,
+            TimeSpan timeout)
         {
             while (true)
             {
                 var startTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                var value = sink.ExpectValue();
+
+                Console.WriteLine("testing");
+                var value = sink.ExpectValue(timeout);
+
                 if (predicate(value))
                 {
                     break;
                 }
 
-                if (DateTimeOffset.Now.ToUnixTimeMilliseconds() - startTime > timeout.TotalMilliseconds)
-                {
-                    // XUnit 2.5+ adds Assert.Fail.
-                    Assert.True(false, message);
-                }
+                if (!(DateTimeOffset.Now.ToUnixTimeMilliseconds() - startTime > timeout.TotalMilliseconds)) continue;
+
+                // XUnit 2.5+ adds Assert.Fail.
+                Assert.True(false, message);
+                return;
             }
         }
     }
